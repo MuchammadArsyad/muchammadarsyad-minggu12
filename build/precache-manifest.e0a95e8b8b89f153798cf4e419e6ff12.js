311ea28805c3050d349cd44a8a1f11300d284307
Muchammad Arsyad <arsyad1822@gmail.com>
2020-04-23 11:32:40 +0700

    update

---
+++ build/precache-manifest.e0a95e8b8b89f153798cf4e419e6ff12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df6c1f512859a878366069bc9098ccb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee200499452cedf6e5d4",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c1c1d482682e7b2215a7",
+    "url": "/static/js/2.c5286309.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c5286309.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee200499452cedf6e5d4",
+    "url": "/static/js/main.b6930c87.chunk.js"
+  },
+  {
+    "revision": "8a09b52ace2e903f11e6",
+    "url": "/static/js/runtime-main.47c578db.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
